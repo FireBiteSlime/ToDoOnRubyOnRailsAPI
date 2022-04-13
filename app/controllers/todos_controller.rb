02ba0a73c9068811d817236a6cd3c934577af8d5
Farhad <46007591+FireBiteSlime@users.noreply.github.com>
2022-04-14 01:17:50 +0300

    Update todos_controller.rb
    Change todos_controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,17 +5,16 @@ class TodosController < ApplicationController
     def update
         project = Project.find(todos_params[:project_id])
         todo = project.todos.find(todos_params[:id])
-		todo.isCompleted = !todo.isCompleted
-		todo.save
-		render :json => todo
+	todo.isCompleted = !todo.isCompleted
+	todo.save
+	render :json => todo
     end
   
     def create
-        if (todos_params[:project_id].empty?)
+        if (todos_params[:project_id] < 0)
 	    project = Project.create(title: todos_params[:title])
             Todo.create(text: todos_params[:text], isCompleted: false, project_id: project["id"])
-	    project = Project.find(project["id"])
-  	    render :json => project
+  	    render :json => project.to_json(:include => [:todos])
           else
             todo = Todo.create(text: todos_params[:text], isCompleted: false, project_id: todos_params[:project_id])
             render :json => todo  
@@ -24,6 +23,6 @@ class TodosController < ApplicationController
 
     private
         def todos_params
-            params.permit(:project_id,:title,:id,:text)
+            params.permit(:project_id,:title,:text)
         end
   end
